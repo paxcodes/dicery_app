7497e264b932d24caafabf8e388a77b5fec2ad65
Margret `Pax` Williams <pax.margret@gmail.com>
2020-06-08 23:58:37 -0700

    :art: Pull back down the children of widgets
    We can use `if(condition) Widget()` as mentioned in the article
    https://medium.com/dartlang/making-dart-a-better-language-for-ui-f1ccaf9f546c

--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -11,63 +11,59 @@ class LobbyScreen extends StatelessWidget {
     Map<String, bool> args = ModalRoute.of(context).settings.arguments;
     final bool isOwnedByUser = args['isOwnedByUser'];
 
-    List<Widget> children = [
-      Text("Room F4K3R",
-          style: Theme.of(context).textTheme.headline1.copyWith(
-                fontSize: 50,
-                fontFamily: 'RobotoMono',
-                fontWeight: FontWeight.bold,
-                color: Colors.teal.shade100,
-              )),
-      Text(
-        "by Sean Williams",
-        style: Theme.of(context)
-            .textTheme
-            .subtitle1
-            .copyWith(color: Colors.teal.shade100),
-      ),
-      SizedBox(height: 50),
-      SizedBox(
-        width: 380,
-        child: TypewriterAnimatedTextKit(
-          text: ['waiting for other players'],
-          textStyle: Theme.of(context).textTheme.headline6.copyWith(
-                fontFamily: "RobotoMono",
-                fontSize: 20,
-                fontWeight: FontWeight.w300,
-                color: Colors.teal.shade100,
-              ),
-          speed: Duration(milliseconds: 100),
-          totalRepeatCount: -1,
-        ),
-      ),
-      SizedBox(height: 20),
-      Expanded(
-        child: ListView(
-          children: <Widget>[
-            PlayerCard(emoji: "💯", name: "Dakota"),
-            PlayerCard(emoji: "🎉", name: "Jordan"),
-          ],
-        ),
-      ),
-    ];
-
-    if (isOwnedByUser) {
-      children.add(DiceryIconButton(
-        label: "Everyone's in",
-        onPressed: () {
-          Navigator.pushNamed(context, '/room');
-        },
-        iconData: Icons.forward,
-      ));
-    }
-
     return Container(
       padding: EdgeInsets.all(16),
       color: Theme.of(context).accentColor,
       child: SafeArea(
         child: Column(
-          children: children,
+          children: [
+            Text("Room F4K3R",
+                style: Theme.of(context).textTheme.headline1.copyWith(
+                      fontSize: 50,
+                      fontFamily: 'RobotoMono',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade100,
+                    )),
+            Text(
+              "by Sean Williams",
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .copyWith(color: Colors.teal.shade100),
+            ),
+            SizedBox(height: 50),
+            SizedBox(
+              width: 380,
+              child: TypewriterAnimatedTextKit(
+                text: ['waiting for other players'],
+                textStyle: Theme.of(context).textTheme.headline6.copyWith(
+                      fontFamily: "RobotoMono",
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.teal.shade100,
+                    ),
+                speed: Duration(milliseconds: 100),
+                totalRepeatCount: -1,
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  PlayerCard(emoji: "💯", name: "Dakota"),
+                  PlayerCard(emoji: "🎉", name: "Jordan"),
+                ],
+              ),
+            ),
+            if (isOwnedByUser)
+              DiceryIconButton(
+                label: "Everyone's in",
+                onPressed: () {
+                  Navigator.pushNamed(context, '/room');
+                },
+                iconData: Icons.forward,
+              ),
+          ],
         ),
       ),
     );
