0ddd774391ed84333d4fcaceb668e7b81066f856
Margret `Pax` Williams <pax.margret@gmail.com>
2020-06-10 17:12:35 -0700

    :fire: Remove colours so we can see the defaults

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -14,22 +14,16 @@ class DiceryIconButton extends StatelessWidget {
     return SizedBox(
       width: 275,
       child: RaisedButton.icon(
-        color: isPrimary ? Colors.teal.shade900 : Colors.teal.shade100,
         onPressed: onPressed,
         shape:
             ContinuousRectangleBorder(borderRadius: BorderRadius.circular(20)),
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
         icon: Icon(
           iconData,
-          color: isPrimary ? Colors.white : Colors.teal.shade900,
         ),
         label: Text(
           label,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            color: isPrimary ? Colors.white : Colors.teal.shade900,
-          ),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
         ),
       ),
     );
