59982d85571c64b6c264d35034fc7f5996315c51
Margret `Pax` Williams <pax.margret@gmail.com>
2020-06-16 19:33:36 -0700

    :sparkles: Use a realistic dummy timestamps

--- lib/models/roll_data.dart
+++ lib/models/roll_data.dart
@@ -4,21 +4,19 @@ import 'package:dicery/models/roll_entry.dart';
 import 'package:flutter/cupertino.dart';
 
 class RollData extends ChangeNotifier {
+  static final DateTime now = DateTime.now();
   List<int> _lastRoll;
   final List<RollEntry> _rollEntries = [
     RollEntry(
         player: 'Dakota',
         rolls: [1, 2, 3, 4, 14],
-        date: DateTime(2020, 1, 2, 6, 1)),
-    RollEntry(
-        player: 'Jordan',
-        rolls: [1, 2, 2, 3, 3],
-        date: DateTime(2020, 1, 2, 6, 31)),
+        date: now.subtract(Duration(minutes: 2))),
     RollEntry(
         player: 'Jordan',
         rolls: [6, 2, 3, 4],
-        date: DateTime(2020, 1, 2, 6, 33)),
-    RollEntry(player: 'Dakota', rolls: [6], date: DateTime(2020, 1, 2, 6, 35)),
+        date: now.subtract(Duration(minutes: 5))),
+    RollEntry(
+        player: 'Dakota', rolls: [6], date: now.subtract(Duration(minutes: 6))),
   ];
 
   List<int> get lastRoll => _lastRoll;
