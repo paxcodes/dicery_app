f06b89d599c8db8ec7ec95a3fa46f4eb075727a3
Margret `Pax` Williams <pax.margret@gmail.com>
2020-07-31 19:14:30 -0700

    :sparkles: Add "empty" screen

--- lib/components/dice_roll_history.dart
+++ lib/components/dice_roll_history.dart
@@ -5,24 +5,19 @@ import 'package:dicery/models/roll_entry.dart';
 
 class DiceRollHistory extends StatelessWidget {
   static final DateTime now = DateTime.now();
-  final List<RollEntry> _rollEntries = [
-    RollEntry(
-        player: 'Dakota',
-        rolls: [1, 2, 3, 4, 14],
-        date: now.subtract(Duration(minutes: 2))),
-    RollEntry(
-        player: 'Jordan',
-        rolls: [6, 2, 3, 4],
-        date: now.subtract(Duration(minutes: 5))),
-    RollEntry(
-        player: 'Dakota', rolls: [6], date: now.subtract(Duration(minutes: 6))),
-  ];
+  final List<RollEntry> _rollEntries = [];
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _rollEntries.length,
-      itemBuilder: (context, index) => RollEntryListItem(_rollEntries[index]),
-    );
+    return _rollEntries.isEmpty
+        ? Text(
+            'No one has rolled the dice yet.',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          )
+        : ListView.builder(
+            itemCount: _rollEntries.length,
+            itemBuilder: (context, index) =>
+                RollEntryListItem(_rollEntries[index]),
+          );
   }
 }
