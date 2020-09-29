1a2a9732ff465461799240d6743b80b299a24fb6
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-29 13:41:56 -0700

    :building_construction: Listen to stream manually
    ...so we can have access to the StreamSubscription and be able
    to pause/resume the stream when app pauses/resume.

--- lib/components/lobby_stream.dart
+++ lib/components/lobby_stream.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-import 'package:dicery/utilities/lobby.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
 import 'package:dicery/components/buttons/everyoneisin_button.dart';
 import 'package:dicery/components/player_card.dart';
-import 'package:dicery/components/stream_listenable_builder.dart';
+import 'package:dicery/utilities/lobby.dart';
 
 class LobbyStream extends StatefulWidget {
   const LobbyStream({
@@ -26,69 +25,39 @@ class LobbyStream extends StatefulWidget {
 
 class _LobbyStreamState extends State<LobbyStream> {
   final _client = http.Client();
-  Future<http.StreamedResponse> streamedResponseFuture;
   final players = <String>[];
+  bool _streamHasError = false;
+  StreamSubscription _streamSubscription;
 
   @override
   void initState() {
-    streamedResponseFuture = Lobby.Subscribe(_client, widget.roomCode);
     super.initState();
+    streamPlayersJoining();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<http.StreamedResponse>(
-      future: streamedResponseFuture,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final streamedResponse = snapshot.data;
-          return StreamListenableBuilder<String>(
-            stream: streamedResponse.stream.toStringStream(),
-            afterDisconnectedListener: (snapshot) {},
-            afterDoneListener: (snapshot) {
-              Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  '/room',
-                  // Remove all screens below
-                  (Route<dynamic> route) => false,
-                  arguments: {
-                    'roomOwner': widget.roomOwner,
-                    'roomCode': widget.roomCode,
-                  });
-            },
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text('Error!');
-              }
-              if (snapshot.hasData) {
-                final data = Lobby.InterpretData(snapshot.data);
-                if (data is List<String>) {
-                  players.insertAll(0, data);
-                }
-              }
-              return Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: players.length,
-                        itemBuilder: (context, index) =>
-                            PlayerCard(emoji: '✨', name: players[index]),
-                      ),
-                    ),
-                    if (widget.isOwnedByUser)
-                      EveryonesInButton(
-                          playerCount: players.length,
-                          roomOwner: widget.roomOwner,
-                          roomCode: widget.roomCode),
-                  ],
-                ),
-              );
-            },
-          );
-        }
-        return Text('');
-      },
+    if (_streamHasError) {
+      return Expanded(child: Text('Error!'));
+    }
+
+    return Expanded(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: players.length,
+              itemBuilder: (context, index) =>
+                  PlayerCard(emoji: '✨', name: players[index]),
+            ),
+          ),
+          if (widget.isOwnedByUser)
+            EveryonesInButton(
+                playerCount: players.length,
+                roomOwner: widget.roomOwner,
+                roomCode: widget.roomCode),
+        ],
+      ),
     );
   }
 
@@ -96,5 +65,33 @@ class _LobbyStreamState extends State<LobbyStream> {
   void dispose() {
     _client.close();
     super.dispose();
+  }
+
+  void streamPlayersJoining() async {
+    var response = await Lobby.Subscribe(_client, widget.roomCode);
+    var stream = response.stream.toStringStream();
+    _streamSubscription = stream.listen(
+      (data) {
+        final newPlayers = Lobby.InterpretData(data);
+        if (newPlayers.isNotEmpty) {
+          setState(() => players.insertAll(0, newPlayers));
+        }
+      },
+      onDone: exitLobby,
+      onError: (err) => setState(() => _streamHasError = true),
+    );
+  }
+
+  void exitLobby() {
+    Navigator.pushNamedAndRemoveUntil(
+      context,
+      '/room',
+      // Remove all screens below.
+      (route) => false,
+      arguments: {
+        'roomOwner': widget.roomOwner,
+        'roomCode': widget.roomCode,
+      },
+    );
   }
 }
