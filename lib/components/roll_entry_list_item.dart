7484bcca5da8df3a271b20a993eb93414415afba
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-30 14:05:08 -0700

    :lipstick: Add spacing in-between RollEntryListItem

--- lib/components/roll_entry_list_item.dart
+++ lib/components/roll_entry_list_item.dart
@@ -11,51 +11,54 @@ class RollEntryListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
 
-    return ListTile(
-      leading: SizedBox(
-        width: 80,
-        child: Text(
-          rollEntry.player,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: PlayerColourizer.getTextColor(rollEntry.player),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: ListTile(
+        leading: SizedBox(
+          width: 80,
+          child: Text(
+            rollEntry.player,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: PlayerColourizer.getTextColor(rollEntry.player),
+            ),
           ),
         ),
-      ),
-      title: Wrap(
-        spacing: 12,
-        runSpacing: 10,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: <Widget>[
-          Text(
-            rollEntry.rolls.join(' '),
-            style: TextStyle(
-              wordSpacing: 15,
-              fontFamily: 'RobotoMono',
-              fontSize: 20,
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: themeData.primaryColor,
-                borderRadius: BorderRadius.all(Radius.circular(3))),
-            padding: const EdgeInsets.all(6.0),
-            child: Text(
-              rollEntry.rolls.reduce((a, b) => a + b).toString(),
+        title: Wrap(
+          spacing: 12,
+          runSpacing: 10,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: <Widget>[
+            Text(
+              rollEntry.rolls.join(' '),
               style: TextStyle(
+                wordSpacing: 15,
                 fontFamily: 'RobotoMono',
-                fontWeight: FontWeight.bold,
                 fontSize: 20,
-                color: themeData.backgroundColor,
               ),
             ),
-          ),
-        ],
-      ),
-      subtitle: Timeago(
-        builder: (_, value) => Text(value),
-        date: rollEntry.date,
+            Container(
+              decoration: BoxDecoration(
+                  color: themeData.primaryColor,
+                  borderRadius: BorderRadius.all(Radius.circular(3))),
+              padding: const EdgeInsets.all(6.0),
+              child: Text(
+                rollEntry.rolls.reduce((a, b) => a + b).toString(),
+                style: TextStyle(
+                  fontFamily: 'RobotoMono',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: themeData.backgroundColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+        subtitle: Timeago(
+          builder: (_, value) => Text(value),
+          date: rollEntry.date,
+        ),
       ),
     );
   }
