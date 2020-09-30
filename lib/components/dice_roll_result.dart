a1e40687ec4b0a3a177c0ba4f37ed9817e7497d4
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-30 13:39:16 -0700

    :sparkles: Add total in DiceRollResult

--- lib/components/dice_roll_result.dart
+++ lib/components/dice_roll_result.dart
@@ -18,21 +18,39 @@ class DiceRollResult extends StatelessWidget {
               style: themeData.textTheme.bodyText1,
             ))
           : Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
               children: <Widget>[
-                ..._diceRolls.map((int num) => Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        num.toString(),
-                        style: TextStyle(
-                          fontFamily: 'RobotoMono',
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: num == 1
-                              ? themeData.accentColor
-                              : themeData.primaryColor,
-                        ),
+                ..._diceRolls.map(
+                  (int num) => Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      num.toString(),
+                      style: TextStyle(
+                        fontFamily: 'RobotoMono',
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: num == 1
+                            ? themeData.accentColor
+                            : themeData.primaryColor,
                       ),
-                    ))
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: themeData.primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(3)),
+                  ),
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(_diceRolls.reduce((a, b) => a + b).toString(),
+                      style: TextStyle(
+                        fontFamily: 'RobotoMono',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: themeData.backgroundColor,
+                      )),
+                ),
               ],
             ),
     );
