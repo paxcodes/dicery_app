c0a6c488086a6a97f388438fb82dafec8fb69c8e
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-30 13:57:44 -0700

    :sparkles: Add dice roll total in RollEntryListItem

--- lib/components/roll_entry_list_item.dart
+++ lib/components/roll_entry_list_item.dart
@@ -9,6 +9,8 @@ class RollEntryListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+
     return ListTile(
       leading: SizedBox(
         width: 80,
@@ -21,13 +23,35 @@ class RollEntryListItem extends StatelessWidget {
           ),
         ),
       ),
-      title: Text(
-        rollEntry.rolls.join(' '),
-        style: TextStyle(
-          wordSpacing: 15,
-          fontFamily: 'RobotoMono',
-          fontSize: 20,
-        ),
+      title: Wrap(
+        spacing: 12,
+        runSpacing: 10,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: <Widget>[
+          Text(
+            rollEntry.rolls.join(' '),
+            style: TextStyle(
+              wordSpacing: 15,
+              fontFamily: 'RobotoMono',
+              fontSize: 20,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: themeData.primaryColor,
+                borderRadius: BorderRadius.all(Radius.circular(3))),
+            padding: const EdgeInsets.all(6.0),
+            child: Text(
+              rollEntry.rolls.reduce((a, b) => a + b).toString(),
+              style: TextStyle(
+                fontFamily: 'RobotoMono',
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: themeData.backgroundColor,
+              ),
+            ),
+          ),
+        ],
       ),
       subtitle: Timeago(
         builder: (_, value) => Text(value),
